364a14691f5ff125bf55b63ec980b5a645d5e53f
Nitrohedge21 <ersanefe21@gmail.com>
2024-02-29 23:01:29 +0000

    Moved the project to the repo

---
+++ MyProject/Source/MyProject/NinjaMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NinjaCharacterMovementComponent.h"
+#include "NinjaMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UNinjaMovementComponent : public UNinjaCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
