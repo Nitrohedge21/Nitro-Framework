2e13eda31fca214b5b3ef4113bd57ed073282c34
Nitrohedge21 <ersanefe21@gmail.com>
2025-04-27 17:53:02 +0100

    Preparing the project
    - Will be moving the project files from the old one into the new one.
    - This project is being portal from UE 5.0 to UE 5.3
    - Hopefully nothing breaks :pray:

---
+++ Source/NitroFramework/NitroFrameworkGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NitroFrameworkGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANitroFrameworkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANitroFrameworkGameMode();
+};
+
+
+
