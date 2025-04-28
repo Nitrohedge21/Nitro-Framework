97b46d3858a00d702cd3f9f2043b4ad102ef8483
Nitrohedge21 <ersanefe21@gmail.com>
2025-04-28 20:47:25 +0100

    Setting things up

--- Source/NitroFramework/NitroFrameworkGameMode.cpp
+++ Source/NitroFramework/NitroFrameworkGameMode.cpp
@@ -1,13 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "NitroFrameworkGameMode.h"
-#include "NitroFrameworkCharacter.h"
+#include "NitroBaseCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ANitroFrameworkGameMode::ANitroFrameworkGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_NitroBaseCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
